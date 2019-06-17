e09b25895ab99ebaa1aca91ebf92d4d0604574e9
Jeff Cedilla <spaghetticoderapps@gmail.com>
2019-06-16 22:22:07 -0700

    clean up code

--- PetAppointmentScheduler/View Controllers/AppointmentDetailViewController.h
+++ PetAppointmentScheduler/View Controllers/AppointmentDetailViewController.h
@@ -19,6 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (strong, nonatomic) NSMutableArray<AppointmentList *> *appointmentLists;
 @property (strong, nonatomic) Appointment *appointment;
+
 + (NSString *) segueIdentifier;
 
 @end
